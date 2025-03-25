188630051900dab86c7274561153e643594facce
lklab <alexeibatic@gmail.com>
2025-03-25 21:19:45 +0900

    git 경로를 github desktop 내에 설치된 git을 사용하도록 수정

--- setup_hooks_win.bat
+++ setup_hooks_win.bat
@@ -1,14 +1,8 @@
 @echo off
+setlocal enabledelayedexpansion
+
 set "RED=0C"
 set "GREEN=0A"
-
-where git >nul 2>&1
-if errorlevel 1 (
-    color %RED%
-    echo Git is not available. Please install Git and try again.
-    pause
-    exit /b 1
-)
 
 if not exist "tools\python\python.exe" (
     color %RED%
@@ -17,9 +11,30 @@ if not exist "tools\python\python.exe" (
     exit /b 1
 )
 
-git config core.hooksPath .githooks-win
+set "USERNAME=%USERNAME%"
+set "GITHUB_PATH=C:\Users\%USERNAME%\AppData\Local\GitHubDesktop"
 
-color %GREEN%
-echo Git hooks path successfully configured to .githooks-win
+set "LATEST_APP="
+for /f "delims=" %%A in ('dir "%GITHUB_PATH%\app-*" /b /ad-h /o-n') do (
+    set "LATEST_APP=%%A"
+    goto :found
+)
+:found
+
+set "GIT_PATH=%GITHUB_PATH%\%LATEST_APP%\resources\app\git\cmd\git.exe"
+
+if exist "%GIT_PATH%" (
+    color %GREEN%
+    "%GIT_PATH%" config core.hooksPath .githooks-win
+    echo Git hooks path successfully configured to .githooks-win
+    pause
+    exit /b 0
+) else (
+    color %RED%
+    echo Git is not available. Please install Git and try again.
+    pause
+    exit /b 1
+)
+
+endlocal
 pause
-exit /b 0
